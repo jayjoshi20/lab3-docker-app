45041395ff0fcca3e138ea46c009ae41e820c39e
EC2 Default User <ec2-user@ip-172-31-90-87.ec2.internal>
2025-09-21 23:27:25 +0000

    Updated message to v2

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def index():
-    return "Hello from Lab 3 via GitHub Actions → ECR (v1)!\n"
+    return "Hello from Lab 3 via GitHub Actions → ECR (v2)!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
